5fc42fa0eb480d2c2ab7f63ba82ff699a0ef7e65
13546721611 <19991008hyd>
2019-09-17 20:16:26 +0800

    20190917

--- src/main/java/cn/bdqn/pnback/dao/GoodsDao.java
+++ src/main/java/cn/bdqn/pnback/dao/GoodsDao.java
@@ -15,4 +15,24 @@ public interface GoodsDao {
      * 获取商品信息通过page,从第一个开始，每页显示几条数据
      */
     List<Goods> getGoodsPage(@Param("index") int index,@Param("pageCount") int pageCount);
+    /**
+     * 删除
+     */
+    Integer del(@Param("gId") Integer gId);
+    /**
+     * 添加
+     */
+    Integer add(Goods goods);
+    /**
+     * 修改
+     */
+    Integer upd(Goods goods);
+    /**
+     * 根据id查
+     */
+    Goods getGoodsById(@Param("gId") Integer gId);
+    /**
+     * 模糊查
+     */
+    List<Goods> getGoodsByName(@Param("gName") String gName);
 }
