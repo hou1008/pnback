9801cf5f7fc8cece497ed98bbac6b1156cb13155
侯琦 <houqi1214>
2019-09-29 14:03:43 +0800

    sss

--- src/main/java/cn/bdqn/pnback/controller/ConsultsController.java
+++ src/main/java/cn/bdqn/pnback/controller/ConsultsController.java
@@ -5,14 +5,21 @@ import cn.bdqn.pnback.service.ConsultsService;
 import cn.bdqn.pnback.util.Page;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/consults")
@@ -47,8 +54,10 @@ public class ConsultsController {
     @RequestMapping("/add")
     public String add(Consults consults){
         int res = consultsService.add(consults);
-        return JSON.toJSONString(res);
-    }
+       return JSON.toJSONString(res);
+   }
+
+
     //id查
 
     @RequestMapping("getConsults/{cId}")
@@ -85,5 +94,9 @@ public class ConsultsController {
             }
         }
     }
+
+
+
+
 
 }
